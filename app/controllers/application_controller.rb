3565efaf12d78cb13bf6ead3eb367ce22e1e8366
ParkerBacall <pbacall2@gmail.com>
2020-02-12 13:05:51 -0700

    fix authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::API
             token = authorization_header.split(" ")[1]
             secret = Rails.application.secrets.secret_key_base[0]
             decoded_token = JWT.decode(token, secret)[0]
-            @user = User.find(decoded_token["user_id"])
+                        @user = User.find(decoded_token["user_id"])
+            render json: @user
          end
     end
 end
